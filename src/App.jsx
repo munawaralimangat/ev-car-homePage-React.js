5e51cc6fe39a99959716d01b1de74cd6d07fcae1
Munawar Ali M <munawaralimangat@gmail.com>
2024-03-03 01:22:22 +0530

    setup app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Background from './components/Background/Background';
+
+const App = () => {
+  let heroData = [
+    {text1:"Dive in to",text2:"what you love"},
+    {text1:"Indulge your",text2:"passions"},
+    {text1:"Give in to",text2:"your passions"}
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const [playStatus,setPlayStatus] = useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+    </div>
+  )
+}
+
+export default App;
