76a5e9f0338ae69521ccd613b0ebef579fe91e6a
Munawar Ali M <munawaralimangat@gmail.com>
2024-03-31 14:54:33 +0530

    css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import pause_icon from '../../assets/pause_icon.png';
 const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus})=>{
     return(
         <div className='hero'>
-            <div>
+            <div className='hero-text'>
             <p>{heroData.text1}</p>
             <p>{heroData.text2}</p>
             </div>
